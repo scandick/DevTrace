c78544e3712aa697114df470b26351a52c64fa04
= <=>
2026-04-29 07:53:44 +0300

    chore: updated services contract: analysis -> candidate_found

--- backend/app/service/analysis.py
+++ backend/app/service/analysis.py
@@ -1,3 +1,5 @@
+from typing import TypedDict
+
 from backend.app.service.req_extract import RequirementData, extract_reqs
 from backend.app.service.code_extract import CodeChunkData, extract_code_chunks
 
@@ -6,12 +8,24 @@ from backend.app.service.code_extract import CodeChunkData, extract_code_chunks
 text_from_req_file = ''
 text_from_code_file = ''
 
+class AnalyzeData(TypedDict):
+    """
+    Класс кандидата (соответствие требование - искходный код).
+
+    Args: dict (TypeDict): словарь с однозначным заданием кандидата следующего содержания: 
+                            requirements (list[RequirementData]) - 
+                            code_chunks (list[CodeChunkData]) -
+                            # TODO 
+                            
+    """
+    requirements : list[RequirementData]
+    code_chunks : list[CodeChunkData] 
+    ### TODO 
+
 def analyze(text_from_req_file: str, text_from_code_file: str):
-
-    requirements : list[RequirementData] = extract_reqs(text_from_req_file)
-    code_chunks : list[CodeChunkData] = extract_code_chunks(text_from_code_file)
-
-    return {
-        "reqs": requirements,
-        "code_chunks": code_chunks
-    }
+    return AnalyzeData(
+            {
+            "requirements" : extract_reqs(text_from_req_file),
+            "code_chunks": extract_code_chunks(text_from_code_file)
+        }
+    )
