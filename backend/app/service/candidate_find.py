44b50b46ea1a09ea7cdeadac84106aceba0e20ac
= <=>
2026-04-30 16:59:39 +0300

    feat: add finding candidates by best score, set candidates status

--- backend/app/service/candidate_find.py
+++ backend/app/service/candidate_find.py
@@ -16,18 +16,22 @@ class CandidateData(TypedDict):
                             requirement_key (str) - Идентификатор требования.
                             requirement_text: str) - Текст требования.
                             code_chunk_name: (str | None) - Название фрагмента кода (опционально)
+                            code_chunk_content (str | None) - Содержание фрагмента кода (опционально)
                             similarity_score: (float) - Степень смысловой схожести
                             system_status: (str) - candidate_found / ambiguous / no_candidate_found
     """
     requirement_key: str
     requirement_text: str
     code_chunk_name: str | None
+    code_chunk_content : str | None
     similarity_score: float
-    system_status: str
+    candidate_status: str
 
 
 def find_candidates(analyze_data : AnalyzeData) -> list[CandidateData]:
     """
+    Реализует поиск кандидатов на соответствие среди требований и фрагментов кода, опираясь на оценку "схожести", кандидат - пара с высшей оценкой.
+
     1. Проверка на пустые списики
     2. Все текста для единого словаря: requirements_texts + code_texts
     3. Трэин для vectorizer + fit_transform (на одном пространстве признаков для требований и кодовых фрагментов)
@@ -39,6 +43,8 @@ def find_candidates(analyze_data : AnalyzeData) -> list[CandidateData]:
        - определить status
        - append в results
     7. return results
+
+    Returns: Информацию о кандидате в формате CandidateData
     """
     requirements : list[RequirementData] = analyze_data['requirements']
     if len(requirements) < 1:
@@ -68,7 +74,7 @@ def find_candidates(analyze_data : AnalyzeData) -> list[CandidateData]:
 
     # временно в двойном цикле, требует оптимизации 
     # подсчёт схожести каждого вектора требования с каждым вектором исходного кода
-    res = []
+    req_code_score_list = []
     for vec_req in X_req:
         similarity_to_req = []
         for vec_code in X_code:
@@ -78,7 +84,31 @@ def find_candidates(analyze_data : AnalyzeData) -> list[CandidateData]:
 
             score = cosine_similarity(req_part, code_part)
             similarity_to_req.append(score)
-        res.append(similarity_to_req)
+        req_code_score_list.append(similarity_to_req)
+
+    all_candidates : list[CandidateData] = []
+
+    # Найти best score
+    for req_ind, code_scores in enumerate(req_code_score_list):
+        req_best_score = max(code_scores)
+
+        requirement = requirements[req_ind]
+
+        chunk_ind = code_scores.index(req_best_score)
+        code_chunk = code_chunks[chunk_ind]
+
+        # определение статуса кандидата (временно грубая оценка по score)
+
+        candidate = CandidateData(
+    {"requirement_key": requirement["requirement_key"] if req_best_score > 0 else None,
+            "requirement_text": requirement["text"] if req_best_score > 0 else None,
+            "code_chunk_name": code_chunk['name'] if req_best_score > 0 else None,
+            "code_chunk_content" : code_chunk['content'] if req_best_score > 0 else None,
+            "similarity_score": req_best_score,
+            "candidate_status": set_candidate_status(req_best_score)})
+
+        all_candidates.append(candidate)
+    
 
 
     #similarity_matrix = cosine_similarity(X_req, X_code)
@@ -87,10 +117,17 @@ def find_candidates(analyze_data : AnalyzeData) -> list[CandidateData]:
     result = {
         "names": names.tolist(),
         "shape": list(X.shape),
-        "res" : res
+        "candidates_list" : all_candidates
     }
     return result 
 
-def set_candidate_status():
-    pass
+def set_candidate_status(score: float) -> str: 
+    """
+    Опреляет статус кандидата по грубой оценке score
+    """
+    if score >= 0.2:
+        return "candidate_found"
+    if score >= 0.1:
+        return "ambiguous"
+    return "no_candidate"
 
