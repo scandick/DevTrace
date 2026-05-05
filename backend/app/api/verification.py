6c024172edbdefa868fbdf23c50b1143ec3cc32c
= <=>
2026-05-05 10:09:14 +0300

    feat: add verification_matrix fastapi endpoint and router

--- backend/app/api/verification.py
+++ backend/app/api/verification.py
@@ -1,24 +1,17 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
-# импорт таблциц sql и pydantic-схем
-from backend.app import models, schemas 
-# выдача сессии бд
+from backend.app import models
 from backend.app.database import get_db
-
-# получения функции вывода резултатов анализа
 from backend.app.service.analysis import analyze
 
-from backend.app.service.candidate_find import find_candidates
-
 # роутер эндпоинтов для analysis
-router = APIRouter(prefix="/projects/{project_id}/analyze", 
-                   tags=["analysis"])
+router = APIRouter(prefix="/projects/{project_id}/verification-matrix", tags=["verification"])
 
 @router.post("")
-def run_analysis(project_id: int,
-                  db: Session = Depends(get_db)):
-    """Получение текста из загруженных документов, выделение из них требований и исходного кода.
+def get_verification_matrix(project_id: int, db: Session = Depends(get_db)):
+    """
+    Реализует функционал формирования матрицы верификации. На данном этапе по сути копирует analysis.py. Но выводит лишь его часть
 
     Args:
         project_id (int): Схема создания проекта.
@@ -28,8 +21,7 @@ def run_analysis(project_id: int,
         HTTPException: "Project not found", "Requirements document not found", "Souce code document not found".
 
     Returns: 
-        
-    
+
     """
     # select * from projects pr where pr.id = project_id limit 1
     project = db.query(models.Project).filter(models.Project.id == project_id).first()
@@ -48,4 +40,4 @@ def run_analysis(project_id: int,
     if code_doc is None:
         raise HTTPException(status_code=400, detail="Souce code document not found")
     
-    return analyze(req_doc.content, code_doc.content)
+    return analyze(req_doc.content, code_doc.content)["verification_matrix"]
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 
 from backend.app.database import Base, engine
-from backend.app.api import projects, documents, analysis
+from backend.app.api import projects, documents, analysis, verification
 
 # Первоначальная инициализация таблиц (из Base) в сесси БД
 Base.metadata.create_all(bind=engine)
@@ -10,6 +10,7 @@ app = FastAPI(title="DevTrace")
 app.include_router(projects.router)
 app.include_router(documents.router)
 app.include_router(analysis.router)
+app.include_router(verification.router)
 
 @app.get("/")
 def read_root():
